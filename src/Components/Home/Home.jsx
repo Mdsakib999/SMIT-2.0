89e6db1fe9159b9f1ae1a30afed962727ff92cd6
Mdsakib999 <sakibabrar999@gmail.com>
2024-08-29 18:01:08 -0700

    footer and contact added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import "./Home.css";
+import Contact from '../Contact';
 
 const Home = () => {
     return (
         <div>
-            <h1>This is home.</h1>
+            <h1>This is home. this the great home ever </h1>
+
+            {/* Contact Section */}
+            <Contact></Contact>
         </div>
     );
 };
