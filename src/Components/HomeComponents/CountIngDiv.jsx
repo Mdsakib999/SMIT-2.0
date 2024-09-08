c561fd8266170e4c56568d22485b6f64df5e631c
safi islam <118913244+safiislam@users.noreply.github.com>
2024-09-08 13:14:37 +0600

    testimonials add

--- src/Components/HomeComponents/CountIngDiv.jsx
+++ src/Components/HomeComponents/CountIngDiv.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+
 import CountUp from "react-countup";
 import { FaRegDotCircle } from "react-icons/fa";
 import img from "../../assets/WorldMapPNG.png";
