ba49b036335ae8547a49d2e7e90348cfd480157f
safi islam <118913244+safiislam@users.noreply.github.com>
2024-09-16 18:07:42 +0600

    add

--- src/routs.jsx
+++ src/routs.jsx
@@ -7,7 +7,6 @@ import Service from './Pages/Service/Service';
 import ServiceDetails from './Pages/Service/ServiceDetails';
 import About from './Pages/About/About';
 import Career from './Pages/Career/Career';
-import CareerDetails from './Pages/Career/JobDetails';
 import JobDetails from './Pages/Career/JobDetails';
 
 
